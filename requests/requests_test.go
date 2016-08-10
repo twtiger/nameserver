897b6d93915759c9f32e8d8f83292d25634c4071
Chelsea H. Komlo <chelsea.komlo@gmail.com>
2016-08-09 22:17:34 -0500

    C | remove unused test helper; but we should test setting & reading all headers in a single test

--- requests/requests_test.go
+++ requests/requests_test.go
@@ -11,32 +11,7 @@ type RequestsSuite struct{}
 
 var _ = Suite(&RequestsSuite{})
 
-func getTestHeaders() map[FieldName][]byte {
-	return map[FieldName][]byte{
-		ID:      []byte{0x00, 0x01},
-		QR:      []byte{0x00, 0x01},
-		OPCODE:  []byte{0x00, 0x01},
-		AA:      []byte{0x00, 0x01},
-		TC:      []byte{0x00, 0x01},
-		RD:      []byte{0x00, 0x01},
-		RA:      []byte{0x00, 0x01},
-		Z:       []byte{0x00, 0x00},
-		RCODE:   []byte{0x00, 0x00},
-		QDCOUNT: []byte{0x00, 0x01},
-		ANCOUNT: []byte{0x00, 0x01},
-		NSCOUNT: []byte{0x00, 0x01},
-		ARCOUNT: []byte{0x00, 0x01},
-	}
-}
-
-func buildTestHeaders() ([]byte, map[FieldName][]byte) {
-	h := getTestHeaders()
-	data := make([]byte, 12)
-	data[0] = h[ID][0]
-	data[1] = h[ID][1]
-	data[2] = byte(uint16(h[QR][1]) << uint16(7))
-	return data, h
-}
+// TODO we should test setting and reading all of the headers in a single test
 
 func (s *RequestsSuite) TestReadIDFromUDPHeaders(c *C) {
 	data := make([]byte, 12)
