17c6a3ac4077e89b4348b504adb9684ce42f58c2
Chelsea H. Komlo <chelsea.komlo@gmail.com>
2016-08-17 13:35:07 -0500

    R & Y & C | structure around DNS database

--- database/database.go
+++ database/database.go
@@ -1,5 +1,19 @@
 package database
 
-func Lookup(name string, qType, qClass int) ([]Record, error) {
-	// TODO
+// Persistance is an interface for a type that is able to lookup queries for a DNS record
+// TODO make better interface name
+type Persistance interface {
+	Lookup(name string, qType, qClass int) ([]Record, error)
 }
+
+// Database holds DNS Zones and their corresponding Records in a tree-like structure
+type Database struct{}
+
+// Lookup takes a record name, type, and class, and returns a list of corresponding records and an error
+func (p *Database) Lookup(name string, qType, qClass int) ([]Record, error) {
+	// TODO
+	return nil, nil
+}
+
+// Record is a DNS record
+type Record struct{}
